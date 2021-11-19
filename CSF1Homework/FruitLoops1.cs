5c45c9a75b0dd668bcb7d6263d6f0f5177778994
Janet Lester <janet.lester1@outlook.com>
2021-11-19 16:36:55 -0600

    Added FruitLoops1 class solution

---
+++ CSF1Homework/FruitLoops1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoops1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Froot Loops 1");
+            Console.Title = "FROOT LOOPS 1";
+
+            string[] fruitLoops1 = { "apple", "bananna", "orange", "apricot", "blueberry", "plum" };
+
+            for (int i = 0; i < fruitLoops1.Length; i++)
+            {
+                Console.WriteLine(fruitLoops1[i]);
+            }
+
+
+
+
+        }//end SVM  
+    }//end class
+}//end namespace
